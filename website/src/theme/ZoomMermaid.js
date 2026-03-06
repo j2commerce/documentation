9c0cc128a9d2e33bcffe2e602bef80d2fbc635f1
Olivier <olivier.buisard@simplifyyourweb.com>
2026-03-06 15:34:17 -0500

    Change class name

--- website/src/theme/ZoomMermaid.js
+++ website/src/theme/ZoomMermaid.js
@@ -1,7 +1,7 @@
 import mediumZoom from 'medium-zoom';
 
 export default function zoomMermaid() {
-    mediumZoom('.mermaid svg', {
+    mediumZoom('.docusaurus-mermaid-container svg', {
         background: 'rgba(0,0,0,0.8)',
         margin: 24
     });
